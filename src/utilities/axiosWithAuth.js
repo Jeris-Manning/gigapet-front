e9b546420e6326d82a7a7b946367f101e82e967e
Jeris Manning <the.goose.moose@gmail.com>
2020-04-01 16:33:13 -0600

    changed db used

--- src/utilities/axiosWithAuth.js
+++ src/utilities/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem("token")
   const user_id = localStorage.getItem("user_id")
   return axios.create({
-    baseURL: "https://gigapets-winter-2019.herokuapp.com",
+    baseURL: "https://meal-monster.herokuapp.com/",
     headers: {
       Authorization: token,
       user_id: user_id
